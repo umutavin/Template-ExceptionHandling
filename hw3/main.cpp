db4bb2ea572dfcb59c2c78194c0d7abd643b94a1
umutavin <umutavin@gmail.com>
2018-05-12 13:32:16 +0300

    Initial Commit

---
+++ hw3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw3
+//
+//  Created by Umut Avin on 12.05.2018.
+//  Copyright © 2018 avin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
